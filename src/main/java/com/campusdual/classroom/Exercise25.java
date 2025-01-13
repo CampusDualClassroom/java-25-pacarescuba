4f83242451076bbc9958f8b6b38ca09065d4ef21
pacarescuba <94604499+pacarescuba@users.noreply.github.com>
2025-01-13 13:37:18 +0100

    Added solution for Exercise25

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -1,20 +1,29 @@
 package com.campusdual.classroom;
 
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Exercise25 {
-    public static Stack<String> createStack() {
+  public static Stack<String> createStack() {
+    Stack<String> stack = new Stack<>();
+    stack.push("Smith");
+    stack.push("Montessori");
+    stack.push("Peralta");
+    stack.push("House");
+    return stack;
+  }
 
+  public static void printAndEmptyStack(Stack<String> stack) {
+    while (!stack.isEmpty()) {
+      System.out.println(stack.remove(stack.size() - 1));
     }
+  }
 
-    public static void printAndEmptyStack(Stack<String> stack) {
-
-    }
-
-    public static void main(String[] args) {
-
-    }
-
+  public static void main(String[] args) {
+    printAndEmptyStack(createStack());
+  }
 
 
 }
